a7b8dfd445e9d7e7202e3632cba6cae77d2f39fe
Hieu Trung <trunghieusn2002@gmail.com>
2024-01-10 00:45:40 +0700

    First commit

---
+++ init.lua
@@ -0,0 +1,42 @@
+vim.g.mapleader = ' '
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('plugins')
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.wo.number = true
+
+ vim.o.mouse = 'a'
+
+ vim.o.breakindent = true
+
+ vim.o.undofile = true
+
+ vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.wo.signcolumn = 'yes'
+
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = 'menuone,noselect'
+
+vim.o.termguicolors = true
+
+vim.wo.relativenumber = true
+
+vim.opt.guicursor = 'i:block'
