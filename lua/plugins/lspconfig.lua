4023c707076c09f1b841991d3888d9b86ef3b3fb
Hieu Trung Nguyen <hieu-trung.nguyen@adnovum.vn>
2025-04-18 12:05:06 +0700

    Update config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -76,12 +76,19 @@ return {
         --  define the property 'filetypes' to the map in question.
         local servers = {
             -- clangd = {},
-            -- gopls = {},
+            gopls = {},
             -- pyright = {},
             -- rust_analyzer = {},
-            tsserver = {},
+            jdtls = {},
+            ts_ls = {},
+            angularls = {},
             cssls = {},
+            gradle_ls = {},
             html = { filetypes = { 'html', 'twig', 'hbs' } },
+            kotlin_language_server = {},
+            bashls = {},
+            denols = {},
+            pylsp = {},
 
             lua_ls = {
                 Lua = {
@@ -116,6 +123,18 @@ return {
                     filetypes = (servers[server_name] or {}).filetypes,
                 }
             end,
+        }
+
+        local nvim_lsp = require('lspconfig')
+        nvim_lsp.denols.setup {
+            on_attach = on_attach,
+            root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+        }
+
+        nvim_lsp.ts_ls.setup {
+            on_attach = on_attach,
+            root_dir = nvim_lsp.util.root_pattern("package.json"),
+            single_file_support = false
         }
     end
 }
